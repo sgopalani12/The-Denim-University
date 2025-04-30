6a4cf3a7b0d8c9e9562499ea34a77f6f21d0e79d
sgopalani12 <sgopalani12@gmail.com>
2025-04-29 20:33:03 -0400

    added js code

--- searchbar-filter.js
+++ searchbar-filter.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const searchFilter = document.querySelectorAll('input[name="color"]');
+    const colorFilter = document.querySelectorAll('input[name="color"]');
+    const searchBar = document.querySelectorAll('input[type="text"]');
+    c
+
+    [...colorFilter].forEach((input) => {
+        console.log(`Searching: ${input.name} - ${input.value}`)
+    })
+
+    console.log(`SEARCHING HERE: ${searchBar.value}`)
 })
