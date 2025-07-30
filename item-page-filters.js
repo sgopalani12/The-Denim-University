fc9c302daf7d1c9cdacf7e86d0e5ac9d46992f59
sgopalani12 <sgopalani12@gmail.com>
2025-07-29 23:04:04 -0400

    chore(item-page-filter.js): working on being able to click one box at a time

--- item-page-filters.js
+++ item-page-filters.js
@@ -1,21 +1,24 @@
-export async function clickCheckbox() {
-document.addEventListener("DOMContentLoaded", () => {
-    const waistFilters = document.querySelectorAll('label [name="waist"]');
-    const lengthFilters = document.querySelectorAll('label [name="length"]');
-    const colorFilters = document.querySelectorAll('label [name="color"]');
+// export async function clickCheckbox() {
+// document.addEventListener("DOMContentLoaded", () => {
+//     const waistFilters = document.querySelectorAll('label [name="waist"]');
+//     const lengthFilters = document.querySelectorAll('label [name="length"]');
+//     const colorFilters = document.querySelectorAll('label [name="color"]');
 
-    // Log all filters on page load
-    [...waistFilters, ...lengthFilters, ...colorFilters].forEach((input) => {
-        console.log(`Filter available: ${input.name} - ${input.value}`);
-    });
-});
+//     // Log all filters on page load
+//     [...waistFilters, ...lengthFilters, ...colorFilters].forEach((input) => {
+//         console.log(`Filter available: ${input.name} - ${input.value}`);
+//     });
+// });
 
-const checkbox = document.querySelector("input[name=checkbox]");
-checkbox.addEventListener('change', function () {
-    if (this.checked) {
-        console.log("Checkbox is checked..");
-    } else {
-        console.log("Checkbox is not checked..");
-    }
-});
-}
+// const checkbox = document.querySelector("input[name=checkbox]");
+// checkbox.addEventListener('change', function () {
+//     if (this.checked) {
+//         console.log("Checkbox is checked..");
+//     } else {
+//         console.log("Checkbox is not checked..");
+//     }
+// });
+// }
+
+
+document.querySelectorAll 
